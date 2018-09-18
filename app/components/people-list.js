a08c43b843b1f1b82e0562a96a2ba33b99c8ec3e
Daniel schwartz <mdblakestj@gmail.com>
2018-09-18 18:02:14 -0400

    first real commit

---
+++ app/components/people-list.js
@@ -0,0 +1,12 @@
+// import Component from '@ember/component';
+//
+// export default Component.extend({
+// });
+
+<h2>{{title}}</h2>
+
+<ul>
+  {{#each people as |person|}}
+    <li>{{person}}</li>
+  {{/each}}
+</ul>
